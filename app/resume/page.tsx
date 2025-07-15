780c5f243da34ac5d99aa2f8fe70a7e85285f9f2
akshayrachakonda <akshayrachakonda5@gmail.com>
2025-07-15 23:38:22 +0530

    resume downloaded updated

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -27,12 +27,15 @@ export default function ResumePage() {
 
       <motion.a
         href="/assets/akshay-resume.pdf"
-        download
+        download="Akshay_Rachakonda_Resume"
+        target="_blank" // ← ensures mobile opens it
+        rel="noopener noreferrer"
         whileHover={{ scale: 1.1, boxShadow: '0 0 20px #8b5cf6' }}
         className="bg-purple-700 px-8 py-4 rounded-full text-lg font-semibold shadow-lg transition-all duration-300 hover:bg-purple-800"
       >
         🚀 Download Resume
-      </motion.a>
+    </motion.a>
+
 
       <motion.iframe
         src="/assets/akshay-resume.pdf"
@@ -41,7 +44,6 @@ export default function ResumePage() {
         animate={{ opacity: 1, scale: 1 }}
         transition={{ delay: 0.5, duration: 0.8 }}
         />
-
 
     </main>
   );
